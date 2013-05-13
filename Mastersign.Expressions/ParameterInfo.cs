6716a5fde7bae88d8938483f4c432646f55e76c5
mastersign <dev@mastersign.de>
2013-05-13 08:44:05 +0200

    fixed XML comment

--- Mastersign.Expressions/ParameterInfo.cs
+++ Mastersign.Expressions/ParameterInfo.cs
@@ -24,7 +24,7 @@ namespace de.mastersign.expressions
         internal ParameterExpression Expression { get; private set; }
 
         /// <summary>
-        /// Initializes a new instance of <see cref="Parameter"/>.
+        /// Initializes a new instance of <see cref="ParameterInfo"/>.
         /// </summary>
         /// <param name="name">The name of the parameter.</param>
         /// <param name="type">The type of the parameter.</param>
