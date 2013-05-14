85ba993d6a278f56cb49739990abf6b5e664d6f6
mastersign <dev@mastersign.de>
2013-05-14 13:51:43 +0200

    added GrammarCapabilities to allow controlling language support

---
+++ Mastersign.Expressions/language/GrammarCapabilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace de.mastersign.expressions.language
+{
+    /// <summary>
+    /// This enumeration contains all configuration modes for the grammar.
+    /// </summary>
+    public enum GrammarCapabilities
+    {
+        /// <summary>
+        /// The basic language with variables, parameters, operators, functions, groups.
+        /// </summary>
+        Basic,
+
+        /// <summary>
+        /// <see cref="Basic"/> with support for reading public properties and fields.
+        /// </summary>
+        /// <remarks>
+        /// This capability adds a certain object oriented flavor to the grammar,
+        /// that breaks with the initially pure functional approach.
+        /// </remarks>
+        MemberRead,
+    }
+}
