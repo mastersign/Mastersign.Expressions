4f9416e2240e0dba7e0181122694b7c59a6c56d3
mastersign <dev@mastersign.de>
2013-05-14 11:34:11 +0200

    added interface IRightPart for inconclusive right hand tokens

---
+++ Mastersign.Expressions/language/IRightPart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace de.mastersign.expressions.language
+{
+    internal interface IRightPart
+    {
+        ExpressionElement BuildExpressionElement(ExpressionElement leftPart);
+    }
+}
